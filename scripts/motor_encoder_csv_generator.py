40c340243bcdefee0ecf7e1499edee37fcc3910d
Spectre <zmontross@gmail.com>
2021-05-04 15:26:58 -0600

    Arrived at 0.000245 dV/PWM after testing motors and comparing results. See 'motor_encoder_data_check.ods' for details!

--- scripts/motor_encoder_csv_generator.py
+++ scripts/motor_encoder_csv_generator.py
@@ -1,7 +1,7 @@
 
 import serial
 
-from numpy import array
+from numpy import array, trim_zeros
 
 import time as Time
 
@@ -65,40 +65,20 @@ class SerialCommunicator():
         return rx
 
 
-def main(args=None):
+def MotorTest(sc=SerialCommunicator(), motor_number=1, motor_pwm_max=400, motor_pwm_step=25, test_period_sec=15):
+    
+    print("Testing Motor {}".format(motor_number))
 
-    m1_data = {}
-    m2_data = {}
-
+    motor_data = []
     wheel_diameter_meters = 0.04186
-
     encoder_counts_per_rev = 2248.86
-
     encoder_counts_per_meter = encoder_counts_per_rev / (wheel_diameter_meters * pi)
 
-    sc = SerialCommunicator()
-    
-    sc.serial_send(SerialCommands.stop_m1)
-    sc.serial_send(SerialCommands.stop_m2)
-    sc.serial_send(SerialCommands.reset_enc1)
-    sc.serial_send(SerialCommands.reset_enc2)
+    sc.serial_send('sm{} 0'.format(motor_number))
+    Time.sleep(0.5)
+    for pwm in range(0, (motor_pwm_max + motor_pwm_step), motor_pwm_step):
 
-    print('Initialized.')
-    # sc.serial_send(SerialCommands.do_beep2)
-    # Time.sleep(0.25)
-    # sc.serial_send(SerialCommands.do_beep3)
-
-
-    speed_max = 400
-    speed_step = 25
-
-    print('Beginning test with Motor 1 (right)')
-
-    sc.serial_send('sm1 0')
-    Time.sleep(0.25)
-    for speed in range(0, (speed_max + speed_step), speed_step):
-
-        print("Testing Speed={}".format(speed))
+        print("Testing PWM={}".format(pwm))
 
         encoder_count_start = 0
         encoder_count_end = 0
@@ -110,9 +90,9 @@ def main(args=None):
 
         velocity = 0.0
 
-        # Stop, and ramp-up to the target speed
-        for ramp in range(speed, (speed + speed_step), 1):
-            sc.serial_send('sm1 {}'.format(ramp))
+        # Ramp-up to the target speed, allow motor to maintain speed for a short period.
+        for ramp in range(pwm, (pwm + motor_pwm_step), 1):
+            sc.serial_send('sm{} {}'.format(motor_number, ramp))
             Time.sleep(0.1)
         Time.sleep(0.5)
 
@@ -120,119 +100,102 @@ def main(args=None):
         time_start = Time.time()
 
         # Snapshot encoder initial
-        _, encoder_count_start, _ = (sc.serial_send(SerialCommands.query_encoders)).split(':')
+        encoder_count_start = (sc.serial_send(SerialCommands.query_encoders)).split(':')[motor_number]
         
         # Run for a while...
-        Time.sleep(15)
+        Time.sleep(test_period_sec)
 
         # Snapshot ending time
         time_end = Time.time()
 
         # Snapshot encoder final
-        _, encoder_count_end, _ = (sc.serial_send(SerialCommands.query_encoders)).split(':')
+        encoder_count_end= (sc.serial_send(SerialCommands.query_encoders)).split(':')[motor_number]
 
         ds = (int(encoder_count_end) - int(encoder_count_start)) / encoder_counts_per_meter
         dt = time_end - time_start
 
         velocity = ds / dt
 
-        m1_data[speed] = velocity
-
-        # print('Result ~ \tMotor Speed={}\t\t\tVelocity= {} m/s'.format(
-        #     speed, velocity
-        # ))
+        motor_data.append([pwm, velocity])
         
-
     # Ramp-down to stopping, then ensure a stop occurs
-    for ramp in range(speed_max, -speed_step, -1):
-        sc.serial_send('sm1 {}'.format(ramp))
+    for ramp in range(motor_pwm_max, -motor_pwm_step, -1):
+        sc.serial_send('sm{} {}'.format(motor_number, ramp))
         Time.sleep(0.1)
-    sc.serial_send('sm1 0')
+    sc.serial_send('sm{} 0'.format(motor_number))
     Time.sleep(0.5)
 
+    return motor_data
 
-    print('M1 Test complete.')
-    print(m1_data.items())
 
-    ################################################################################################
+def main(args=None):
+    
+    try:
+        sc = SerialCommunicator()
+        
+        sc.serial_send(SerialCommands.stop_m1)
+        sc.serial_send(SerialCommands.stop_m2)
+        sc.serial_send(SerialCommands.reset_enc1)
+        sc.serial_send(SerialCommands.reset_enc2)
+
+    except:
+        print('An error occurred while establishing serial comms. Aborting test.')
+        sys.exit(1)
+
+    
+    pwm_max = 400
+    pwm_step = 25
+    test_period = 20
+
+    print('Beginning test with Motor 1 (right).')
+    m1_velocities = MotorTest(sc, motor_number=1, motor_pwm_max=pwm_max, motor_pwm_step=pwm_step, test_period_sec=test_period)
 
     print('Beginning test with Motor 2 (Left)')
+    m2_velocities = MotorTest(sc, motor_number=2, motor_pwm_max=pwm_max, motor_pwm_step=pwm_step, test_period_sec=test_period)
 
-    sc.serial_send('sm2 0')
-    Time.sleep(0.25)
-    for speed in range(0, (speed_max + speed_step), speed_step):
+    print('Analyzing results for Motor 1...')
+    m1_deltas = array([0.00] * len(m1_velocities))
+    for v in range(1, len(m1_velocities)):
+        delta = m1_velocities[v][1] - m1_velocities[v-1][1]
+        m1_deltas[v] = delta
 
-        print("Testing Speed={}".format(speed))
+    print('Analyzing results for Motor 2...')
+    m2_deltas = array([0.00] * len(m2_velocities))
+    for v in range(1, len(m2_velocities)):
+        delta = m2_velocities[v][1] - m2_velocities[v-1][1]
+        m2_deltas[v] = delta
 
-        encoder_count_start = 0
-        encoder_count_end = 0
-        time_start = 0.0
-        time_end = 0.0
-
-        ds = 0.0  # Distance delta
-        dt = 0.0  # Time delta
-
-        velocity = 0.0
-
-        # Stop, and ramp-up to the target speed
-        for ramp in range(speed, (speed + speed_step), 1):
-            sc.serial_send('sm2 {}'.format(ramp))
-            Time.sleep(0.1)
-        Time.sleep(0.5)
-
-        # Snapshot starting time
-        time_start = Time.time()
-
-        # Snapshot encoder initial
-        _, _, encoder_count_start = (sc.serial_send(SerialCommands.query_encoders)).split(':')
+    fname = 'motor_encoder_data.csv'
+    print('Writing data to file "{}"'.format(fname))
+    with open(fname, 'w') as fhandle:
+        csvwriter = csv.writer(fhandle, delimiter=',')
         
-        # Run for a while...
-        Time.sleep(15)
+        csvwriter.writerow(['Motor PWM', 'M1 Velocity, m/s', 'M1 Delta From Last', '', 'M1 Delta Average', '', 'Test Period, s'])
+        csvwriter.writerow([m1_velocities[0][0], m1_velocities[0][1], m1_deltas[0], '', m1_deltas.mean(), '', test_period])
+        for i in range(1, len(m1_velocities)):
+            row = [m1_velocities[i][0], m1_velocities[i][1], m1_deltas[i]]
+            csvwriter.writerow(row)
+        csvwriter.writerow([])
 
-        # Snapshot ending time
-        time_end = Time.time()
+        csvwriter.writerow(['Motor PWM', 'M2 Velocity, m/s', 'M2 Delta From Last', '', 'M2 Delta Average', '', 'Test Period, s'])
+        csvwriter.writerow([m2_velocities[0][0], m2_velocities[0][1], m2_deltas[0], '', m2_deltas.mean(), '', test_period])
+        for i in range(1, len(m2_velocities)):
+            row = [m2_velocities[i][0], m2_velocities[i][1], m2_deltas[i]]
+            csvwriter.writerow(row)
+        csvwriter.writerow([])
 
-        # Snapshot encoder final
-        _, _, encoder_count_end = (sc.serial_send(SerialCommands.query_encoders)).split(':')
+        csvwriter.writerow(['Average of Averages, M1 Delta & M2 Delta'])
+        motor_delta_avg_of_avgs = (trim_zeros(m1_deltas).mean() + trim_zeros(m2_deltas).mean()) / 2
+        csvwriter.writerow([motor_delta_avg_of_avgs])
+        csvwriter.writerow([])
 
-        ds = (int(encoder_count_end) - int(encoder_count_start)) / encoder_counts_per_meter
-        dt = time_end - time_start
+        csvwriter.writerow(['Velocity Increase (m/s), per PWM unit'])
+        motor_vel_inc_per_pwm = motor_delta_avg_of_avgs / pwm_step
+        csvwriter.writerow([motor_vel_inc_per_pwm])
+        csvwriter.writerow([])
 
-        velocity = ds / dt
-
-        m2_data[speed] = velocity
-
-        # print('Result ~ \tMotor Speed={}\t\t\tVelocity= {} m/s'.format(
-        #     speed, velocity
-        # ))
-        
-
-    # Ramp-down to stopping, then ensure a stop occurs
-    for ramp in range(speed_max, -speed_step, -1):
-        sc.serial_send('sm2 {}'.format(ramp))
-        Time.sleep(0.1)
-    sc.serial_send('sm2 0')
-    Time.sleep(0.5)
-
-
-    print('M2 Test complete.')
-    print(m2_data.items())
-
-    ################################################################################################
-
-    with open('motor_encoder_relationship.csv', 'w') as csvdata:
-
-        csvwriter = csv.writer(csvdata, delimiter=',')
-
-        csvwriter.writerow(['Motor Speed', 'M1 Velocity, m/s', 'M2 Velocity, m/s'])
-
-        for i in range(0, (speed_max+speed_step), speed_step):
-            csvwriter.writerow([i, m1_data.get(i), m2_data.get(i)])
-
-        csvdata.close()
+        fhandle.close()
 
 
 if __name__ == '__main__':
     main()
-
-    
